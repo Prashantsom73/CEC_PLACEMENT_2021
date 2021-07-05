2e1d5de7f7920e0f058b117212303fe186a1a032
Prashant som <75539746+Prashantsom73@users.noreply.github.com>
2021-07-05 15:48:00 +0530

    DAY 4

---
+++ PLACEMENT_PREP/DAY_4_2_07_2021/Array_Partition.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int arrayPairSum(vector<int>& nums){
+    vector<int> v;
+  
+  
+    
+
+}
+int main(){
+    vector<int> arr = {1,4,3,2};
+    int result= arrayPairSum(arr);
+    for(int i=0;i<result.size();i++){
+        cout<<result[i];
+    }
+    
+}
