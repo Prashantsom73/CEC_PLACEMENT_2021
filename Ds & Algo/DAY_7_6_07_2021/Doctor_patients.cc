ec3b0a9ff7060489d7b08fe9cbdbdf3507779097
Prashant som <75539746+Prashantsom73@users.noreply.github.com>
2021-07-06 11:46:46 +0530

    DAY 7

---
+++ Ds & Algo/DAY_7_6_07_2021/Doctor_patients.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void Doctor_patients(vector<vector<int>>& nums) {
+// int sum=0;
+for (int i = 0; i < nums.size(); i++) {
+    for (int j = 0; j < nums[0].size(); j++) {
+        if(nums[j][i]==0)
+            if(nums[j][ i-1<0? 0 :i-1 ]==1)
+                cout<<j <<" ";
+        }     
+    }
+}
+
+int main(){
+    vector<vector<int>> v={{1,1,1,1},{1,1,0,0},{1,0,0,0},{1,1,0,0}};
+    Doctor_patients(v);
+    
+}
---
+++ Ds & Algo/DAY_7_6_07_2021/new.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> transpose(vector<vector<int>>& nums) {
+
+    int m=0;
+	for (int i = 0; i < nums.size(); i++) {
+        int min=*min_element(nums[i].begin(),nums[i].end());
+		for (int j = 0; j < nums[0].size(); j++) {
+            int max=*max_element(nums[j].begin(),nums[j].end());
+            if(min>max){
+                m=min;
+            }
+            if(m==max){
+                cout<<m;
+                break;
+            }
+		}
+	}
+    // cout<<m;
+ }
+
+int main(){
+    vector<vector<int>> v={{1,10,4,2},{9,3,8,7},{15,16,17,12}};
+    vector<vector<int>> result=transpose(v);
+    // for(int i=0;i<result.size();i++){
+    //     for(auto it: result[i]){
+    //         cout<<it<<" ";
+    //     }
+    //         cout<<endl;
+    // }
+
+}
