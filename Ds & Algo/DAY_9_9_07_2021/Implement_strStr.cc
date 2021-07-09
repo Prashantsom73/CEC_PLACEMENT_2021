5a2975efad0abf4b36e70ac9efcd11e95b5c0111
Prashant som <75539746+Prashantsom73@users.noreply.github.com>
2021-07-10 00:06:56 +0530

    DAY 9

--- Ds & Algo/DAY_9_9_07_2021/Implement_strStr.cc
+++ Ds & Algo/DAY_9_9_07_2021/Implement_strStr.cc
@@ -1,11 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int strStr(string haystack, string needle) {
-    int found = haystack.find(needle);
-    return found;
-}
-
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        if(n==0){
+            return 0;
+        }
+        for (int i = 0; i<=m-n; i++) {
+            int j = 0;
+            for (; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
 int main(){
-  string haystack = "hello", needle = "ll" ;
+  string haystack = "ello", needle = "ll" ;
   cout<<strStr(haystack,needle);
 }
--- Ds & Algo/DAY_9_9_07_2021/tempCodeRunnerFile.cc
+++ Ds & Algo/DAY_9_9_07_2021/tempCodeRunnerFile.cc
@@ -1,2 +1,16 @@
-  if(!st.empty())
-    //     st.push_back(s);
+int strStr(string haystack, string needle) {
+    // int found = haystack.find(needle);
+    // return found;
+    int p,temp;
+     if(needle.empty()){
+        return 0;
+    }
+    for(int i=0;i<haystack.size();i++){
+      for(int j=0;j<haystack.size();j++){
+          if(haystack.substr(i,j+1)==needle){
+            return i;
+          }
+      }
+    }   
+    return -1;
+}
