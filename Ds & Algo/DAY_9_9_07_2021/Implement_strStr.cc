29f32362686c03078258157d950c522ac7169713
Prashant som <75539746+Prashantsom73@users.noreply.github.com>
2021-07-10 19:32:28 +0530

    DAY 10

--- Ds & Algo/DAY_9_9_07_2021/Implement_strStr.cc
+++ Ds & Algo/DAY_9_9_07_2021/Implement_strStr.cc
@@ -1,19 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-    int strStr(string haystack, string needle) {
+    // int strStr(string haystack, string needle) {
+    //     int m = haystack.size(), n = needle.size();
+    //     if(n==0){
+    //         return 0;
+    //     }
+    //     for (int i = 0; i<=m-n; i++) {
+    //         int j = 0;
+    //         for (; j < n; j++) {
+    //             if (haystack[i + j] != needle[j]) {
+    //                 break;
+    //             }
+    //         }
+    //         if (j == n) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+       int strStr(string haystack, string needle) {
         int m = haystack.size(), n = needle.size();
-        if(n==0){
-            return 0;
-        }
+
+        int j=0;
         for (int i = 0; i<=m-n; i++) {
-            int j = 0;
-            for (; j < n; j++) {
-                if (haystack[i + j] != needle[j]) {
-                    break;
-                }
-            }
-            if (j == n) {
+            if(haystack[i]==needle[j]){
+
+            if(haystack.substr(i,n)==needle){
                 return i;
+            }
             }
         }
         return -1;
