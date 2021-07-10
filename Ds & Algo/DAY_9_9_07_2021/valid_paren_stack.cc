ded0005004c55c78ee39d27b879b5da51778d102
Prashant som <75539746+Prashantsom73@users.noreply.github.com>
2021-07-10 19:43:46 +0530

    DAY 10

--- Ds & Algo/DAY_9_9_07_2021/valid_paren_stack.cc
+++ Ds & Algo/DAY_9_9_07_2021/valid_paren_stack.cc
@@ -1,20 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isValid(string s) {
-    stack<int> st;
-    for(int i=0;i<s.size();i++){
-        // stack.push()
-        if(s[i]=='{' ||s[i]=='(' ||s[i]=='['){
+   bool isValid(string s) {
+     stack<char> st;
+      for(int i=0;i<s.length();i++)
+      {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[') 
             st.push(s[i]);
-        }
-        else
-            if(st.empty()==true) 
-                return false;
-            else if ((st.top()=='('&& s[i] ==')') || (st.top()=='{'&& s[i]=='}') || (st.top()=='['&& s[i]==']'))
-                return true;
-            else 
-                st.pop();
-        }
+        else if(!st.empty() && st.top() == '(' && s[i] == ')')
+            st.pop();
+        else if(!st.empty() && st.top() == '{' && s[i] == '}')
+            st.pop();
+        else if(!st.empty() && st.top() == '[' && s[i] == ']') 
+            st.pop();
+        else 
+            return false;
+      }
+      if(st.empty()) 
+          return true;
+      else 
+          return false;
     }
 
     
